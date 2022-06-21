ae5d6bcf9bee1d86d3cf6076cb614ee20e9789b8
anastasiyaSarackach <anastasiya.sarakach@gmail.com>
2022-06-21 21:32:03 +0300

    Homework1

---
+++ Homework1/Homework1/Homework1/Helpers.h
@@ -0,0 +1,8 @@
+#pragma once
+#include <cmath>
+
+double SumSquared(double a, double b) 
+{
+	return pow((a + b), 2);
+
+};
---
+++ Homework1/Homework1/Homework1/Homework1.cpp
@@ -0,0 +1,19 @@
+﻿
+
+#include <iostream>
+#include "Helpers.h"
+
+int main()
+{
+    double  a = 0, b = 0;
+    std::cout << "Enter the value a = " << "\n";
+    std::cin >> a;
+
+    std::cout << "Enter the value b = " << "\n";
+    std::cin >> b;
+
+    std::cout << "Answear = " << SumSquared( a, b ) << "\n";
+
+    return 0;
+}
+
