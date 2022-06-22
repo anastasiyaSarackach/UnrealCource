70d0df13b735afb3c8bcaaadeac0f407dcf5482f
anastasiyaSarackach <anastasiya.sarakach@gmail.com>
2022-06-22 20:45:04 +0300

    Homework2

---
+++ Новая папка/HomeWork2/HomeWork2/HomeWork2.cpp
@@ -0,0 +1,13 @@
+﻿#include <string>
+#include <iostream>
+
+int main()
+{
+    std::string hello = "Hello World";
+    std::cout << hello << "\n";
+    std::cout << " size = " << hello.size() << "\n";
+    std::cout << " first symbol = " << hello[0] << "\n";
+    std::cout << " end symbol = " <<hello [hello.size()-1] << "\n";
+    return 0;
+}
+
